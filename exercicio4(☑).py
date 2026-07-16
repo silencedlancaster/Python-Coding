6702278bd78c6adc9defa1d06bf12d5d0fcf1f8e
Richard Hayes M. <analyafernandes22@gmail.com>
2026-07-16 16:05:54 -0300

    Add files via upload
    Pasta 1
     Exercicio 4 de Python.
     Codigo para conversão de km/s para m/s

---
+++ exercicio4(☑).py
@@ -0,0 +1,9 @@
+#pedindo o valor da velocidade em km/s
+velocidade_km = float(input("Digite o valor da velocidade média (km/h): "))
+
+#a conversão de km/s para m/s
+velocidadems = velocidade_km / 3.6
+
+print("A velocidade em m/s: ", f"{velocidadems:.2f}")
+
+

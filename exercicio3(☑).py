15a3ee624a9e9a2c26dabc826ef62efb4c2350c2
Richard Hayes M. <analyafernandes22@gmail.com>
2026-07-16 16:00:44 -0300

    Add files via upload
    Pasta 1
     Exercicio 3 de Python.
     Codigo para fazer a soma e o produto entre duas variáveis

---
+++ exercicio3(☑).py
@@ -0,0 +1,12 @@
+#pedindo o valor das duas variáveis
+numero1 = int(input("Digite o valor do primeiro número: "))
+numero2 = int(input("Digite o valor do segundo número: "))
+
+#soma recebe a soma dos números da variável
+#produto recebe a multiplicação dos números das variáveis
+soma = numero1 + numero2
+produto = numero1 * numero2
+
+#o resultado sendo mostrado na tela
+print("A soma entre esses números: ", f"{soma:.2f}")
+print("O produto entre esses números: ", f"{produto:.2f}")

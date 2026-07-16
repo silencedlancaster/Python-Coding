4addf0b17af0cd5ad639b2b7a65531ec8492124c
Richard Hayes M. <analyafernandes22@gmail.com>
2026-07-16 15:57:07 -0300

    Add files via upload
    Pasta 1
     Exercicio 1 de Python.
     Codigo para descobrir a área de um triângulo

---
+++ exercicio1(☑).py
@@ -0,0 +1,7 @@
+#pedindo o valor das medidas do triângulo para descobrir a sua área
+base = int(input("Digite o valor da base do triângulo: "))
+altura = int(input("Digite o valor da altura do triângulo:"))
+
+area_triangulo = (base + altura) / 2
+
+print("A área do trângulo é: ", area_triangulo)

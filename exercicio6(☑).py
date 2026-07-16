19fa5b9216a6469dadf22c7f475b27db343d0412
Richard Hayes M. <analyafernandes22@gmail.com>
2026-07-16 16:10:07 -0300

    Add files via upload
    Pasta 1
     Exercicio 6 de Python.
     Codigo para descobrir o delta para resolver uma equação de segundo grau

---
+++ exercicio6(☑).py
@@ -0,0 +1,10 @@
+#pedindo o valor das variáveis
+a = int(input("Digite o valor da a: "))
+b = int(input("Digite o valor de b: "))
+c = int(input("Digite o valor de c: "))
+
+#o calculo para descobrir o valor de delta
+delta = b **2 - 4 * a * c
+
+#resultado mostrado na tela
+print("O valor de delta: ", delta)
